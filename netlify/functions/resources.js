ecb398e9d6f687526becf4249bfbffeb779ed72b
Isaac McDaniel <isaac@Isaacs-MacBook-Pro.local>
2022-08-03 22:48:02 -0500

    Changed resources formatting to be parsable by GABSC site

--- netlify/functions/resources.js
+++ netlify/functions/resources.js
@@ -4,7 +4,8 @@ exports.handler = async function () {
     const LOCATION_DOC = 'https://opensheet.elk.sh/1YWymQHAIQnXhGOttbI20zflXY0HVfrvQ2yMcTWjtscw/Locations'
 
     const response = await fetch(LOCATION_DOC)
-    const data = await response.json()
+    const data = {}
+    data.resources = await response.json()
 
     return {
         statusCode: 200,
